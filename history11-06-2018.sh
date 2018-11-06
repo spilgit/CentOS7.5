f5a14f747fb30b1c17623d36bbf01c420f836075
KHMF Technologies <spilgit@users.noreply.github.com>
2018-11-06 14:56:26 -0500

    Create history11-06-2018.sh

---
+++ history11-06-2018.sh
@@ -0,0 +1,46 @@
+    1  ifconfig
+    2  yum list
+    3  yum upgrade
+    4  reboot
+    5  find / -name sshd_config
+    6  nano /etc/ssh/sshd_config
+    7  systemctl reload sshd.service
+    8  yum upgrade
+    9  yum update
+   10  cat /etc/redhat release
+   11  cat /etc/redhat-release
+   12  nano /etc/ssh/sshd_config
+   13  exit
+   14  sudo -i
+   15  exit
+   16  last
+   17  yum -y install setuptool system-config-network* system-config-firewall* system-config-securitylevel-tui system-config-keyboard
+   18  reboot
+   19  find / -name sshd_config
+   20  nano /etc/ssh/sshd_config
+   21  systemctl reload sshd.service
+   22  yum install webmin
+   23  clear
+   24  yum install mc
+   25  mc
+   26  setup
+   27  history
+   28  last
+   29  yum install httpd
+   30  yum install mysqld phpmyadmin
+   31  yum install mysql mysql-server phpmyadmin
+   32  mysql
+   33  systemctl start mysql.service
+   34  mysql
+   35  sudo nano /etc/yum.repos.d/webmin.repo
+   36      wget http://www.webmin.com/jcameron-key.asc
+   37      sudo rpm --import jcameron-key.asc
+   38  sudo yum install webmin
+   39  systemctl start webmin.service
+   40  webmin
+   41  systemctl
+   42  find / -name webmin
+   43  /etc/rc.d/init.d/webmin
+   44  /etc/rc.d/init.d/webmin start
+   45  /etc/rc.d/init.d/webmin status
+   46  history
